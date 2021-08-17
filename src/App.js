ecbd29cd89f4805910498036097b48653dcd216f
Luiz Fernando Rodrigues Sanches <luiz@caliban.local>
2021-08-17 16:55:19 -0300

    adicionando multiplos contadores

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import { useState } from "react";
 
-const frutas = [
-  "pessego",
-  "tomate",
-  "banana",
-  "abacate",
-  "pera",
-  "maçã",
-  "pera",
-];
-
 function App() {
+  /**
+   * 1. Renderizar vários contadores
+   * 2. Um botão para adicionar um contador novo
+   * 3. Input para armazenar a unidade por contador
+   */
+
+  // -> essa é a abordagem por array
+  const [contadores, setContadores] = useState([]);
+
+  // -> implementar abordagem por numero
+  // const [quantidadeContres, setQuantidadeContadores] = useState(0)
+
+  const adicionarContador = () => {
+    // .push(<Contador />) -> altera o array original
+    // .concat(<Contador />) -> nao altera o array original
+    // setContadores()
+    setContadores((contadoresAtuais) => contadoresAtuais.concat(Contador));
+  };
+
+  /**
+   * [function() {}, function(){}]
+   */
+
   return (
     <div>
-      {/* <Contador unidadePorClique={3} /> */}
-
-      <ol>
-        {/* {[<li>pessego</li>, <li>tomate</li>, <li>banana</li>, <li>abacate</li>]} */}
-        {frutas.map((fruta, index) => (
-          <li key={index}>{fruta}</li>
-        ))}
-      </ol>
+      <button onClick={adicionarContador}>Adicionar contador</button>
+      {/* {contadores.map((contador) => contador)} */}
+      {contadores.map((ContadorRenderizado, index) => (
+        <ContadorRenderizado key={index} unidadePorClique={2} />
+      ))}
     </div>
   );
 }
@@ -32,7 +42,8 @@ function Contador(props) {
   const [contador, setContador] = useState(0);
 
   const subtrair = () => {
-    setContador(contador - props.unidadePorClique);
+    // setContador(contador - props.unidadePorClique);
+    setContador((prevState) => prevState - props.unidadePorClique);
     console.log(contador);
   };
 
